1fd152cb2ccccaadaf3f7f37e189ad0afda8847b
Marcello <marcellomarabese@hotmail.com>
2017-01-26 19:15:36 +0000

    Add some simple business logic into Basket and Item classes.

--- src/Mmarab.CqsExample.Models/Basket.cs
+++ src/Mmarab.CqsExample.Models/Basket.cs
@@ -7,5 +7,28 @@ namespace Mmarab.CqsExample.Models
 {
     public class Basket
     {
+        private readonly List<Item> _items; 
+        public Guid Id { get; }
+
+        public Basket(Guid id)
+        {
+            _items = new List<Item>();
+            Id = id;
+        }
+
+        public void AddItem(Item item)
+        {
+            _items.Add(item);
+        }
+
+        public void DeleteItem(Guid itemId)
+        {
+            _items.RemoveAll(r => r.Id == itemId);
+        }
+
+        public decimal GetTotal()
+        {
+            return _items.Sum(s => s.ItemPrice);
+        }
     }
 }
--- src/Mmarab.CqsExample.Models/Item.cs
+++ src/Mmarab.CqsExample.Models/Item.cs
@@ -1,7 +1,16 @@
-﻿namespace Mmarab.CqsExample.Models
+﻿using System;
+
+namespace Mmarab.CqsExample.Models
 {
     public class Item
     {
+        public Guid Id { get;  }
+        public decimal ItemPrice { get; }
 
+        public Item(Guid id, decimal itemPrice)
+        {
+            Id = id;
+            ItemPrice = itemPrice;
+        }
     }
 }
