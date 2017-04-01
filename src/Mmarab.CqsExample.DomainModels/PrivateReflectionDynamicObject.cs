76c21ad7e2c731f1a54c9e53913b10e201d12347
Marcello <marcellomarabese@hotmail.com>
2017-04-01 13:10:30 +0100

    Private Reflection added to Domain for Events.

---
+++ src/Mmarab.CqsExample.DomainModels/PrivateReflectionDynamicObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+using System.Dynamic;
+using System.Linq;
+
+namespace Mmarab.CqsExample.DomainModels
+{
+    class PrivateReflectionDynamicObject : DynamicObject
+    {
+        private object RealObject { get; set; }
+        private const BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+
+        internal static object WrapObjectIfNeeded(object o)
+        {
+            return new PrivateReflectionDynamicObject() { RealObject = o };
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            result = InvokeMemberOnType(RealObject.GetType(), RealObject, binder.Name, args);
+            result = WrapObjectIfNeeded(result);
+            return true;
+        }
+
+        private static object InvokeMemberOnType(Type type, object target, string name, object[] args)
+        {
+            try
+            {
+                var result = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .Where(m => m.Name == name)
+                .Where(m => m.GetParameters().Length == 1)
+                .Where(m => m.GetParameters().First().ParameterType == args[0].GetType());
+
+                return result.First().Invoke(target, args);
+            }
+            catch (MissingMethodException)
+            {
+                if (type.GetTypeInfo().BaseType != null)
+                {
+                    return InvokeMemberOnType(type.GetTypeInfo().BaseType, target, name, args);
+                }
+
+                return null;
+            }
+        }
+    }
+}
---
+++ src/Mmarab.CqsExample.DomainModels/PrivateReflectionDynamicObjectExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Mmarab.CqsExample.DomainModels
+{
+    public static class PrivateReflectionDynamicObjectExtensions
+    {
+        public static dynamic AsDynamic(this object o)
+        {
+            return PrivateReflectionDynamicObject.WrapObjectIfNeeded(o);
+        }
+    }
+}
